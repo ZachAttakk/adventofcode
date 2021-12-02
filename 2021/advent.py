738439750b49b0b07604d3439436f3b2bbf26846
Zach Groenewald <zach.groenewald@gmail.com>
2021-12-02 10:56:52 +0200

    Day 2 Part 1

--- 2021/advent.py
+++ 2021/advent.py
@@ -3,11 +3,12 @@ import os
 from timeit import default_timer as timer
 from os import path
 from zUtils.utils import *
+from typing import List
 
 data: list[str] = []
 
 # FILENAME FOR INPUT DATA
-INPUT_FILENAME = "advent.txt"
+INPUT_FILENAME = "day02.txt"
 
 
 def get_data(filename) -> list[str]:
@@ -32,23 +33,35 @@ if (data == []):
     printDisaster("NO FILE")
 
 # HERE WE GO
-# Day 1 Part 2:
-# Check how many times the number gets bigger,
-# when the number is made up of the last 3 numbers.
+# Day 2 Part 1:
+# Function that processes inputs and spits out new coordinates
+# Then we multiply them
 
 
-def last3(index: int) -> int:
-    if index < 2:
-        return -1  # sanity check
-    return int(data[index]) + int(data[index-1]) + int(data[index-2])
+def move(instruction: str) -> List[int]:
+    _a, _b = instruction.split(' ')
+    _command: str = _a
+    _amount: int = int(_b)
+    _dx: int = 0
+    _dy: int = 0
+
+    if _command == "up":
+        _dy -= _amount
+    elif _command == "down":
+        _dy += _amount
+    elif _command == "forward":
+        _dx += _amount
+
+    return [_dx, _dy]
 
 
-counter: int = 0
-for i in range(3, len(data)):
-    # because we step back 1 in the condition,
-    # we need to start high enough so the last 3 begins at 0
-    if last3(i) > last3(i-1):  # gets bigger
-        counter += 1
+position: List[int] = [0, 0]
+for i in data:
+    _delta = move(i)
+    position[0] += _delta[0]
+    position[1] += _delta[1]
 
-printGood(counter)
+printDebug(f"Final position: {position[0]}, {position[1]}")
+printGood(position[0]*position[1])
+
 printOK("Time: %.2f seconds" % (timer()-start_time))
