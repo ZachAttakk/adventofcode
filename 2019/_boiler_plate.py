14190e2a33c982b030a3154cb6976cc4aaf4554b
Zach Groenewald <zach.groenewald@gmail.com>
2022-11-14 12:25:39 +0200

    2019 update boiler plate file

--- 2019/_boiler_plate.py
+++ 2019/_boiler_plate.py
@@ -2,34 +2,35 @@ import sys
 import os
 from timeit import default_timer as timer
 from os import path
-from zUtils import utils as zUtils
+from zUtils.utils import *
 
-data = []
+data: list[str] = []
 
-def get_data(filename):
-  if path.exists(filename):
-    f=open(filename, "r")
-    if f.mode == 'r':
-      _data =f.readlines()
-      f.close()
-      return _data
-  else: return -1
 
-  #INIT
+def get_data(filename) -> list[str]:
+    _data: list[str] = []
+    if path.exists(filename):
+        f = open(filename, "r")
+        if f.mode == 'r':
+            _data: list[str] = f.read().splitlines()
+            f.close()
+    return _data
+
+
+# INIT
 # Code for startup
 start_time = timer()
-if len(sys.argv) < 2: filename = "advent06.txt"
-else: filename = sys.argv[1]
+if len(sys.argv) < 2:
+    filename = "advent.txt"
+else:
+    filename = sys.argv[1]
 data = get_data(filename)
-if (data == -1):
-  print("NO FILE")
+if (data == []):
+    printDisaster("NO FILE")
 
 array = []
 
-#HERE WE GO
+# HERE WE GO
 
 
-
-
-
-zUtils.printOK("Time: %.2f seconds" % (timer()-start_time))
+printOK("Time: %.2f seconds" % (timer()-start_time))
