9dd1136a2a5c814be5b2da4d9d1628c265d2cc1c
Zach Groenewald <zach.groenewald@gmail.com>
2022-12-06 09:18:40 +0200

    2022 let day 6 part 2 use function from part 1

--- 2022/day06_2.py
+++ 2022/day06_2.py
@@ -1,29 +1,12 @@
 import sys
 from timeit import default_timer as timer
 from zUtils.utils import *
+from day06_1 import find_start
 
 data: list[str] = []
 
 # FILENAME FOR INPUT DATA
 INPUT_FILENAME: str = "06.txt"
-
-
-def find_start(stream: str, length: int) -> int:
-    """Checks for a continuous segment of [length] characters that are all different.
-
-    Args:
-        stream (str): Data stream to check
-        length (int): Length of sequence to find
-
-    Returns:
-        int: index of last digit after [length] different characters
-    """
-    for i in range(length, len(stream)):
-        # character exists in the last 4 digits
-        check_range = set(stream[i-length:i])
-        if len(check_range) == length:
-            return i
-    return -1
 
 
 def main():
