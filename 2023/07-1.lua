9e6139805bc278808bf74916b162a5d50b0f8d0e
Zach Groenewald <zach.groenewald@gmail.com>
2023-12-07 16:28:03 +0200

    2023 Day 7

---
+++ 2023/07-1.lua
@@ -0,0 +1,82 @@
+local utils = require "utils"
+
+local input_file = "07.txt"
+local inputs = utils.lines_from(input_file)
+
+-- DO THE CODE HERE
+
+CARD_LIST = { "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A" }
+CARD_STRENGTH = "23456789TJQKA"
+HAND_TYPES = {
+    { 1, 1, 1, 1, 1 },
+    { 2, 1, 1, 1 },
+    { 2, 2, 1 },
+    { 3, 1, 1 },
+    { 3, 2 },
+    { 4, 1 },
+    { 5 }
+}
+
+local function sort_hands(a, b)
+    if a.strength ~= b.strength then
+        return a.strength < b.strength
+    else
+        for i = 1, 5, 1 do
+            local a_c = string.find(CARD_STRENGTH, a.cards:sub(i, i))
+            local b_c = string.find(CARD_STRENGTH, b.cards:sub(i, i))
+            if a_c ~= b_c then
+                return a_c < b_c
+            end
+        end
+    end
+end
+
+local function get_counts(cards)
+    local counts_list = {}
+    for _, v in pairs(CARD_LIST) do
+        local _, count = cards:gsub(v, "")
+        if count > 0 then
+            counts_list[v] = count
+        end
+    end
+
+    --convert to string
+    local counts = {}
+    for _, v in pairs(counts_list) do
+        table.insert(counts, v)
+    end
+
+    table.sort_asc(counts)
+
+    return counts
+end
+
+local function get_strength(counts)
+    for strength, value in pairs(HAND_TYPES) do
+        if table.concat(counts) == table.concat(value) then
+            return strength
+        end
+    end
+end
+
+local hands = {}
+
+for _, value in ipairs(inputs) do
+    local input_split = value:split("%s")
+    local hand = { cards = input_split[1], bid = tonumber(input_split[2]) }
+    hand.counts = get_counts(hand.cards)
+    hand.strength = get_strength(hand.counts)
+    table.insert(hands, hand)
+end
+
+table.sort(hands, sort_hands)
+
+local winnings = {}
+
+for rank, hand in pairs(hands) do
+    table.insert(winnings, hand.bid * rank)
+end
+
+-- OUTPUT THE RESULT
+print("  ")
+print(utils.sum(winnings))
