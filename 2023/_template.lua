60aeaac4a62b081ffd13868c0726ba94ae17eaae
Zach Groenewald <zach.groenewald@gmail.com>
2023-12-14 12:13:48 +0200

    2023 Utils: - Move timestamp to utils - Add colour output

--- 2023/_template.lua
+++ 2023/_template.lua
@@ -3,10 +3,6 @@ local utils = require "utils"
 local input_file = "01.txt"
 local inputs = utils.lines_from(input_file)
 
-local function timestamp()
-    print(string.format("Elapsed time: %.3f seconds", os.clock()))
-end
-
 -- DO THE CODE HERE
 
 
@@ -17,4 +13,4 @@ end
 -- OUTPUT THE RESULT
 print("")
 print("OUTPUT GOES HERE")
-timestamp()
+utils.timestamp()
